000a476eb7c8b520685efdcee91199f67fffb700
joaomarccelino <joao.marcelino.esc@gmail.com>
2022-05-19 22:24:42 -0300

    pwa - part1

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = "pwa-joao";
+const assets = [
+    "/",
+    "/index.html",
+    "/manifest.json",
+    "/styles.css",
+    "/script.js",
+    "/js/quagga.min.js",
+    "js/instalar.js",
+    "/js/app.js"
+];
+
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+    e.waitUntil(
+      caches.open(CACHE_NAME).then((cache) => {
+            console.log('[Service Worker] Caching all: app shell and content');
+        return cache.addAll(assets);
+      })
+    );
+  });
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then(res => {
+            return res || fetch(fetchEvent.request)
+        })
+    )
+})
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
